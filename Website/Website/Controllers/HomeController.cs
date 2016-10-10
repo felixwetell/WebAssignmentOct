5c698ad22f90dc36ee1b1b8120155f32b62d813d
Felix Wetell <felix.wetell@gmail.com>
2016-10-10 11:29:35 +0200

    Can now import properties

--- Website/Website/Controllers/HomeController.cs
+++ Website/Website/Controllers/HomeController.cs
@@ -3,11 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Website.Models;
 
 namespace Website.Controllers
 {
     public class HomeController : Controller
     {
+        List<Property> PropertyList;
+
+        public HomeController()
+        {
+            PropertyList = new List<Property>();
+            PropertyList.Add(new Job() { Title = "Printer", Description = "Preparing paper rolls for printing newspaper." });
+            PropertyList.Add(new Job() { Title = "Play leader", Description = "Leader for groups of mentally challenged people." });
+            PropertyList.Add(new Skill() { Title = "HTML5", Precentage = 80, Description = "I feel secure using HTML5." });
+            PropertyList.Add(new Skill() { Title = "CSS3", Precentage = 80, Description = "Knownledgeable in CSS3." });
+            PropertyList.Add(new Skill() { Title = "C#", Precentage = 50, Description = "Getting better and better." });
+        }
+
         // GET: Home
         public ActionResult Index()
         {
@@ -15,7 +28,7 @@ namespace Website.Controllers
         }
         public ActionResult About()
         {
-            return View();
+            return View(PropertyList);
         }
         public ActionResult Contact()
         {
---
+++ Website/Website/Models/Properties.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Website.Models
+{
+    public class Property
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public override string ToString()
+        {
+            return string.Format("{0}: {1}", Title, Description);
+        }
+    }
+
+    public class Skill : Property
+    {
+        public int Precentage { get; set; }
+        public override string ToString()
+        {
+            return string.Format("{0}: {1}% \n{2}", Title, Precentage, Description);
+        }
+    }
+
+    public class Job : Property { }
+    public class Experience : Property{ }
+}
