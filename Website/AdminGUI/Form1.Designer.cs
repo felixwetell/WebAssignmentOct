0d5b45e4b380fcfade968e4ec8f8b7e3ccb484fc
Felix Wetell <felix.wetell@gmail.com>
2016-10-12 11:49:53 +0200

    Connected DB to AdminGUI

--- Website/AdminGUI/Form1.Designer.cs
+++ Website/AdminGUI/Form1.Designer.cs
@@ -31,24 +31,26 @@
             this.lbxProperties = new System.Windows.Forms.ListBox();
             this.label1 = new System.Windows.Forms.Label();
             this.pnlEdit = new System.Windows.Forms.Panel();
-            this.pnlAdd = new System.Windows.Forms.Panel();
-            this.btnEditSave = new System.Windows.Forms.Button();
-            this.btnEditCancel = new System.Windows.Forms.Button();
+            this.cbxEditChooseProperty = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbxEditDescription = new System.Windows.Forms.TextBox();
+            this.tbxEditTitle = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.btnEdit = new System.Windows.Forms.Button();
+            this.btnEditCancel = new System.Windows.Forms.Button();
+            this.btnEditSave = new System.Windows.Forms.Button();
+            this.pnlAdd = new System.Windows.Forms.Panel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.cbxAddChooseProperty = new System.Windows.Forms.ComboBox();
+            this.tbxAddDescription = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbxAddTitle = new System.Windows.Forms.TextBox();
             this.btnAddCancel = new System.Windows.Forms.Button();
             this.btnAdd = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.tbxEditTitle = new System.Windows.Forms.TextBox();
-            this.tbxEditDescription = new System.Windows.Forms.TextBox();
-            this.tbxAddDescription = new System.Windows.Forms.TextBox();
-            this.tbxAddTitle = new System.Windows.Forms.TextBox();
-            this.cbxAddChooseProperty = new System.Windows.Forms.ComboBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.cbxEditChooseProperty = new System.Windows.Forms.ComboBox();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
+            this.btnLbxClear = new System.Windows.Forms.Button();
+            this.btnLbxAdd = new System.Windows.Forms.Button();
             this.pnlEdit.SuspendLayout();
             this.pnlAdd.SuspendLayout();
             this.SuspendLayout();
@@ -62,6 +64,7 @@
             this.lbxProperties.Name = "lbxProperties";
             this.lbxProperties.Size = new System.Drawing.Size(380, 400);
             this.lbxProperties.TabIndex = 0;
+            this.lbxProperties.SelectedIndexChanged += new System.EventHandler(this.lbxProperties_SelectedIndexChanged);
             // 
             // label1
             // 
@@ -89,40 +92,61 @@
             this.pnlEdit.Size = new System.Drawing.Size(281, 238);
             this.pnlEdit.TabIndex = 2;
             // 
-            // pnlAdd
+            // cbxEditChooseProperty
             // 
-            this.pnlAdd.Controls.Add(this.label7);
-            this.pnlAdd.Controls.Add(this.label6);
-            this.pnlAdd.Controls.Add(this.cbxAddChooseProperty);
-            this.pnlAdd.Controls.Add(this.tbxAddDescription);
-            this.pnlAdd.Controls.Add(this.label3);
-            this.pnlAdd.Controls.Add(this.tbxAddTitle);
-            this.pnlAdd.Controls.Add(this.btnAddCancel);
-            this.pnlAdd.Controls.Add(this.btnAdd);
-            this.pnlAdd.Location = new System.Drawing.Point(715, 57);
-            this.pnlAdd.Name = "pnlAdd";
-            this.pnlAdd.Size = new System.Drawing.Size(281, 238);
-            this.pnlAdd.TabIndex = 3;
+            this.cbxEditChooseProperty.FormattingEnabled = true;
+            this.cbxEditChooseProperty.Location = new System.Drawing.Point(54, 67);
+            this.cbxEditChooseProperty.Name = "cbxEditChooseProperty";
+            this.cbxEditChooseProperty.Size = new System.Drawing.Size(144, 21);
+            this.cbxEditChooseProperty.TabIndex = 10;
             // 
-            // btnEditSave
+            // label5
             // 
-            this.btnEditSave.Font = new System.Drawing.Font("Arial", 12F);
-            this.btnEditSave.Location = new System.Drawing.Point(20, 188);
-            this.btnEditSave.Name = "btnEditSave";
-            this.btnEditSave.Size = new System.Drawing.Size(77, 36);
-            this.btnEditSave.TabIndex = 0;
-            this.btnEditSave.Text = "Save";
-            this.btnEditSave.UseVisualStyleBackColor = true;
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial", 12F);
+            this.label5.Location = new System.Drawing.Point(17, 149);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(92, 18);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Description:";
             // 
-            // btnEditCancel
+            // label4
             // 
-            this.btnEditCancel.Font = new System.Drawing.Font("Arial", 12F);
-            this.btnEditCancel.Location = new System.Drawing.Point(186, 188);
-            this.btnEditCancel.Name = "btnEditCancel";
-            this.btnEditCancel.Size = new System.Drawing.Size(77, 36);
-            this.btnEditCancel.TabIndex = 1;
-            this.btnEditCancel.Text = "Cancel";
-            this.btnEditCancel.UseVisualStyleBackColor = true;
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 12F);
+            this.label4.Location = new System.Drawing.Point(17, 108);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 18);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Title:";
+            // 
+            // tbxEditDescription
+            // 
+            this.tbxEditDescription.Font = new System.Drawing.Font("Arial", 10F);
+            this.tbxEditDescription.Location = new System.Drawing.Point(119, 148);
+            this.tbxEditDescription.Name = "tbxEditDescription";
+            this.tbxEditDescription.Size = new System.Drawing.Size(144, 23);
+            this.tbxEditDescription.TabIndex = 6;
+            this.tbxEditDescription.TextChanged += new System.EventHandler(this.tbxEditDescription_TextChanged);
+            // 
+            // tbxEditTitle
+            // 
+            this.tbxEditTitle.Font = new System.Drawing.Font("Arial", 10F);
+            this.tbxEditTitle.Location = new System.Drawing.Point(119, 109);
+            this.tbxEditTitle.Name = "tbxEditTitle";
+            this.tbxEditTitle.Size = new System.Drawing.Size(144, 23);
+            this.tbxEditTitle.TabIndex = 5;
+            this.tbxEditTitle.TextChanged += new System.EventHandler(this.tbxEditTitle_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 16F);
+            this.label2.Location = new System.Drawing.Point(14, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Edit info:";
             // 
             // btnEdit
             // 
@@ -133,6 +157,98 @@
             this.btnEdit.TabIndex = 2;
             this.btnEdit.Text = "Edit";
             this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnEditCancel
+            // 
+            this.btnEditCancel.Font = new System.Drawing.Font("Arial", 12F);
+            this.btnEditCancel.Location = new System.Drawing.Point(186, 188);
+            this.btnEditCancel.Name = "btnEditCancel";
+            this.btnEditCancel.Size = new System.Drawing.Size(77, 36);
+            this.btnEditCancel.TabIndex = 1;
+            this.btnEditCancel.Text = "Cancel";
+            this.btnEditCancel.UseVisualStyleBackColor = true;
+            this.btnEditCancel.Click += new System.EventHandler(this.btnEditCancel_Click);
+            // 
+            // btnEditSave
+            // 
+            this.btnEditSave.Font = new System.Drawing.Font("Arial", 12F);
+            this.btnEditSave.Location = new System.Drawing.Point(20, 188);
+            this.btnEditSave.Name = "btnEditSave";
+            this.btnEditSave.Size = new System.Drawing.Size(77, 36);
+            this.btnEditSave.TabIndex = 0;
+            this.btnEditSave.Text = "Save";
+            this.btnEditSave.UseVisualStyleBackColor = true;
+            this.btnEditSave.Click += new System.EventHandler(this.btnEditSave_Click);
+            // 
+            // pnlAdd
+            // 
+            this.pnlAdd.Controls.Add(this.label7);
+            this.pnlAdd.Controls.Add(this.label6);
+            this.pnlAdd.Controls.Add(this.cbxAddChooseProperty);
+            this.pnlAdd.Controls.Add(this.tbxAddDescription);
+            this.pnlAdd.Controls.Add(this.label3);
+            this.pnlAdd.Controls.Add(this.tbxAddTitle);
+            this.pnlAdd.Controls.Add(this.btnAddCancel);
+            this.pnlAdd.Controls.Add(this.btnAdd);
+            this.pnlAdd.Location = new System.Drawing.Point(691, 57);
+            this.pnlAdd.Name = "pnlAdd";
+            this.pnlAdd.Size = new System.Drawing.Size(281, 238);
+            this.pnlAdd.TabIndex = 3;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Arial", 12F);
+            this.label7.Location = new System.Drawing.Point(13, 110);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(40, 18);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Title:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial", 12F);
+            this.label6.Location = new System.Drawing.Point(13, 149);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(92, 18);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Description:";
+            // 
+            // cbxAddChooseProperty
+            // 
+            this.cbxAddChooseProperty.FormattingEnabled = true;
+            this.cbxAddChooseProperty.Location = new System.Drawing.Point(44, 67);
+            this.cbxAddChooseProperty.Name = "cbxAddChooseProperty";
+            this.cbxAddChooseProperty.Size = new System.Drawing.Size(144, 21);
+            this.cbxAddChooseProperty.TabIndex = 9;
+            // 
+            // tbxAddDescription
+            // 
+            this.tbxAddDescription.Font = new System.Drawing.Font("Arial", 10F);
+            this.tbxAddDescription.Location = new System.Drawing.Point(122, 148);
+            this.tbxAddDescription.Name = "tbxAddDescription";
+            this.tbxAddDescription.Size = new System.Drawing.Size(144, 23);
+            this.tbxAddDescription.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 16F);
+            this.label3.Location = new System.Drawing.Point(17, 14);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(143, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Add property:";
+            // 
+            // tbxAddTitle
+            // 
+            this.tbxAddTitle.Font = new System.Drawing.Font("Arial", 10F);
+            this.tbxAddTitle.Location = new System.Drawing.Point(122, 109);
+            this.tbxAddTitle.Name = "tbxAddTitle";
+            this.tbxAddTitle.Size = new System.Drawing.Size(144, 23);
+            this.tbxAddTitle.TabIndex = 7;
             // 
             // btnAddCancel
             // 
@@ -154,119 +270,35 @@
             this.btnAdd.Text = "Add";
             this.btnAdd.UseVisualStyleBackColor = true;
             // 
-            // label2
+            // btnLbxClear
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Arial", 16F);
-            this.label2.Location = new System.Drawing.Point(14, 14);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(98, 25);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "Edit info:";
+            this.btnLbxClear.Font = new System.Drawing.Font("Arial", 12F);
+            this.btnLbxClear.Location = new System.Drawing.Point(320, 463);
+            this.btnLbxClear.Name = "btnLbxClear";
+            this.btnLbxClear.Size = new System.Drawing.Size(77, 36);
+            this.btnLbxClear.TabIndex = 13;
+            this.btnLbxClear.Text = "Clear";
+            this.btnLbxClear.UseVisualStyleBackColor = true;
+            this.btnLbxClear.Click += new System.EventHandler(this.btnLbxClear_Click);
             // 
-            // label3
+            // btnLbxAdd
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Arial", 16F);
-            this.label3.Location = new System.Drawing.Point(17, 14);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(143, 25);
-            this.label3.TabIndex = 5;
-            this.label3.Text = "Add property:";
-            // 
-            // tbxEditTitle
-            // 
-            this.tbxEditTitle.Font = new System.Drawing.Font("Arial", 10F);
-            this.tbxEditTitle.Location = new System.Drawing.Point(119, 109);
-            this.tbxEditTitle.Name = "tbxEditTitle";
-            this.tbxEditTitle.Size = new System.Drawing.Size(144, 23);
-            this.tbxEditTitle.TabIndex = 5;
-            // 
-            // tbxEditDescription
-            // 
-            this.tbxEditDescription.Font = new System.Drawing.Font("Arial", 10F);
-            this.tbxEditDescription.Location = new System.Drawing.Point(119, 148);
-            this.tbxEditDescription.Name = "tbxEditDescription";
-            this.tbxEditDescription.Size = new System.Drawing.Size(144, 23);
-            this.tbxEditDescription.TabIndex = 6;
-            // 
-            // tbxAddDescription
-            // 
-            this.tbxAddDescription.Font = new System.Drawing.Font("Arial", 10F);
-            this.tbxAddDescription.Location = new System.Drawing.Point(122, 148);
-            this.tbxAddDescription.Name = "tbxAddDescription";
-            this.tbxAddDescription.Size = new System.Drawing.Size(144, 23);
-            this.tbxAddDescription.TabIndex = 8;
-            // 
-            // tbxAddTitle
-            // 
-            this.tbxAddTitle.Font = new System.Drawing.Font("Arial", 10F);
-            this.tbxAddTitle.Location = new System.Drawing.Point(122, 109);
-            this.tbxAddTitle.Name = "tbxAddTitle";
-            this.tbxAddTitle.Size = new System.Drawing.Size(144, 23);
-            this.tbxAddTitle.TabIndex = 7;
-            // 
-            // cbxAddChooseProperty
-            // 
-            this.cbxAddChooseProperty.FormattingEnabled = true;
-            this.cbxAddChooseProperty.Location = new System.Drawing.Point(44, 67);
-            this.cbxAddChooseProperty.Name = "cbxAddChooseProperty";
-            this.cbxAddChooseProperty.Size = new System.Drawing.Size(144, 21);
-            this.cbxAddChooseProperty.TabIndex = 9;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Arial", 12F);
-            this.label4.Location = new System.Drawing.Point(17, 108);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(40, 18);
-            this.label4.TabIndex = 7;
-            this.label4.Text = "Title:";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Arial", 12F);
-            this.label5.Location = new System.Drawing.Point(17, 149);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(92, 18);
-            this.label5.TabIndex = 8;
-            this.label5.Text = "Description:";
-            // 
-            // cbxEditChooseProperty
-            // 
-            this.cbxEditChooseProperty.FormattingEnabled = true;
-            this.cbxEditChooseProperty.Location = new System.Drawing.Point(54, 67);
-            this.cbxEditChooseProperty.Name = "cbxEditChooseProperty";
-            this.cbxEditChooseProperty.Size = new System.Drawing.Size(144, 21);
-            this.cbxEditChooseProperty.TabIndex = 10;
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Arial", 12F);
-            this.label6.Location = new System.Drawing.Point(13, 149);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(92, 18);
-            this.label6.TabIndex = 11;
-            this.label6.Text = "Description:";
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("Arial", 12F);
-            this.label7.Location = new System.Drawing.Point(13, 110);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(40, 18);
-            this.label7.TabIndex = 11;
-            this.label7.Text = "Title:";
+            this.btnLbxAdd.Font = new System.Drawing.Font("Arial", 12F);
+            this.btnLbxAdd.Location = new System.Drawing.Point(237, 463);
+            this.btnLbxAdd.Name = "btnLbxAdd";
+            this.btnLbxAdd.Size = new System.Drawing.Size(77, 36);
+            this.btnLbxAdd.TabIndex = 12;
+            this.btnLbxAdd.Text = "Add";
+            this.btnLbxAdd.UseVisualStyleBackColor = true;
+            this.btnLbxAdd.Click += new System.EventHandler(this.btnLbxAdd_Click);
             // 
             // AdminGUI
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1338, 796);
+            this.Controls.Add(this.btnLbxClear);
+            this.Controls.Add(this.btnLbxAdd);
             this.Controls.Add(this.pnlAdd);
             this.Controls.Add(this.pnlEdit);
             this.Controls.Add(this.label1);
@@ -306,6 +338,8 @@
         private System.Windows.Forms.ComboBox cbxAddChooseProperty;
         private System.Windows.Forms.TextBox tbxAddDescription;
         private System.Windows.Forms.TextBox tbxAddTitle;
+        private System.Windows.Forms.Button btnLbxClear;
+        private System.Windows.Forms.Button btnLbxAdd;
     }
 }
 
--- Website/AdminGUI/Form1.cs
+++ Website/AdminGUI/Form1.cs
@@ -12,15 +12,75 @@ namespace AdminGUI
 {
     public partial class AdminGUI : Form
     {
-
+        WebsiteDB DataBase = new WebsiteDB();
+        
         public AdminGUI()
         {
             InitializeComponent();
+            GUI(false, false);
+            CheckProperties();
         }
 
         private void AdminGUI_Load(object sender, EventArgs e)
         {
 
         }
+
+        void GUI(bool add, bool edit)
+        {
+            pnlAdd.Visible = add;
+            pnlEdit.Visible = edit;
+        }
+
+        void CheckProperties()
+        {
+            lbxProperties.Items.Clear();
+            foreach (var item in DataBase.Properties)
+            {
+                lbxProperties.Items.Add(item);
+            }
+            lbxProperties.DisplayMember = "Title";
+        }
+
+        private void lbxProperties_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnLbxAdd_Click(object sender, EventArgs e)
+        {
+            GUI(true, false);
+        }
+
+        private void btnLbxClear_Click(object sender, EventArgs e)
+        {
+            lbxProperties.SelectedIndex = -1;
+        }
+
+        private void tbxEditTitle_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbxEditDescription_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEditSave_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEditCancel_Click(object sender, EventArgs e)
+        {
+
+        }
+
     }
 }
